a21c9d88b367f9d1f546bca35a28b2b1ee5be75e
marta.basalo <146773530+mmmbgkez@users.noreply.github.com>
2025-10-08 14:24:00 +0200

    Update MonopolyETSE.java

--- MonopolyETSE.java
+++ MonopolyETSE.java
@@ -5,5 +5,16 @@ public class MonopolyETSE {
     public static void main(String[] args) {
         new Menu();
     }
+
+    /*private void lecturaFichero(String fichero) {
+        try{
+            File f= new File(fichero);
+            Scanner sc = new Scanner(f);
+            sc.hasNextLine();
+        } catch (FileNotFoundException e) {
+            System.out.println("No se ha encontrado el fichero");
+        }
+    }*/
     
 }
+
