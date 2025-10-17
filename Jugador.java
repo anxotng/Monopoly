a4703d532412baeca09e8ca587d2002c61d01dca
marta.basalo <146773530+mmmbgkez@users.noreply.github.com>
2025-10-17 11:42:27 +0200

    Enhance Jugador class with properties and methods
    Added getters, setters, and implemented methods for property management and player status.

--- Jugador.java
+++ Jugador.java
@@ -16,6 +16,7 @@ public class Jugador {
     private int tiradasCarcel; //Cuando está en la carcel, contará las tiradas sin éxito que ha hecho allí para intentar salir (se usa para limitar el numero de intentos).
     private int vueltas; //Cuenta las vueltas dadas al tablero.
     private ArrayList<Casilla> propiedades; //Propiedades que posee el jugador.
+    private Tablero tablero;
 
     //Constructor vacío. Se usará para crear la banca.
     public Jugador() {
@@ -31,28 +32,105 @@ public class Jugador {
         this.avatar = new Avatar(tipoAvatar, this, inicio, avCreados);
     }
 
+    //Getters y setters:
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public Avatar getAvatar() {
+        return avatar;
+    }
+
+    public float getFortuna() {
+        return fortuna;
+    }
+
+    public float getGastos() {
+        return gastos;
+    }
+
+    public boolean estaEnCarcel() {
+        return enCarcel;
+    }
+
+    public int getTiradasCarcel() {
+        return tiradasCarcel;
+    }
+
+    public int getVueltas() {
+        return vueltas;
+    }
+
+    public ArrayList<Casilla> getPropiedades() {
+        return propiedades;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    public void setAvatar(Avatar avatar) {
+        this.avatar = avatar;
+    }
+
+    public void setFortuna(float fortuna) {
+        this.fortuna = fortuna;
+    }
+
+    public void setGastos(float gastos) {
+        this.gastos = gastos;
+    }
+
+    public void setEnCarcel(boolean enCarcel) {
+        this.enCarcel = enCarcel;
+    }
+
+    public void setTiradasCarcel(int tiradasCarcel) {
+        this.tiradasCarcel = tiradasCarcel;
+    }
+
+    public void setVueltas(int vueltas) {
+        this.vueltas = vueltas;
+    }
+    
+    public void setPropiedades(ArrayList<Casilla> propiedades) {
+        this.propiedades = propiedades;
+    }
+
     //Otros métodos:
     //Método para añadir una propiedad al jugador. Como parámetro, la casilla a añadir.
     public void anhadirPropiedad(Casilla casilla) {
+        this.propiedades.add(casilla);
     }
 
     //Método para eliminar una propiedad del arraylist de propiedades de jugador.
     public void eliminarPropiedad(Casilla casilla) {
+        this.propiedades.remove(casilla);
     }
 
     //Método para añadir fortuna a un jugador
     //Como parámetro se pide el valor a añadir. Si hay que restar fortuna, se pasaría un valor negativo.
     public void sumarFortuna(float valor) {
+        this.fortuna += valor;
+        
     }
 
     //Método para sumar gastos a un jugador.
     //Parámetro: valor a añadir a los gastos del jugador (será el precio de un solar, impuestos pagados...).
     public void sumarGastos(float valor) {
+
     }
 
     /*Método para establecer al jugador en la cárcel. 
     * Se requiere disponer de las casillas del tablero para ello (por eso se pasan como parámetro).*/
     public void encarcelar(ArrayList<ArrayList<Casilla>> pos) {
+        this.enCarcel = true; //Marcamos al jugador como encarcelado
+        this.tiradasCarcel = 0; //Reiniciamos el contador de tiradas en la carcel.
+        // Hazme una variable que calcule cuantas tiradas hay desde doonde estoy a la carcel con porcentaje y todo para que salga bien (contando con que van de 0 a 39)
+        int posActual = this.avatar.getLugar().getPosicion();
+        int carcel = tablero.encontrar_casilla("Carcel").getPosicion();
+        int tiradasACarcel = (posActual - carcel + 40) % 40; //Calculamos las tiradas necesarias para llegar a la carcel desde la posición actual.
+        this.avatar.moverAvatar(pos, tiradasACarcel); //Movemos el avatar a la casilla de la carcel.
     }
-
 }
