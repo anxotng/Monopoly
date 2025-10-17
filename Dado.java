f1562534e3fcefc5e2d8a3e2901f2f342ed732c0
marta.basalo <146773530+mmmbgkez@users.noreply.github.com>
2025-10-17 11:41:32 +0200

    Refactor Dado to use Random and add forced roll method
    Updated Dado class to use Random for value generation and added forced roll method.

--- Dado.java
+++ Dado.java
@@ -1,13 +1,28 @@
 package partida;
 
+import java.util.Random;
+
 
 public class Dado {
     //El dado solo tiene un atributo en nuestro caso: su valor.
-    private int valor;
+    private Random valor;
+    
+    //Contructor para inicializar un dado
+    public Dado() {
+        this.valor = new Random();
+    }
 
     //Metodo para simular lanzamiento de un dado: devolverá un valor aleatorio entre 1 y 6.
     public int hacerTirada() {
-        this.valor = (int)(Math.random() * 6) + 1;
+        return valor.nextInt(6) + 1; //Generamos un valor entre 1 y 6
     }
 
+    //Método para forzar el valor de los dados
+    public int tirarDadoForzado(int valorForzado1, int valorForzado2) {
+        if (valorForzado1 < 1 || valorForzado1 > 6 || valorForzado2 < 1 || valorForzado2 > 6) {
+            System.out.println("El valor forzado de los dados debe estar entre 1 y 6.");
+        }
+        int valorTotal = valorForzado1 + valorForzado2;
+        return valorTotal;
+    }
 }
